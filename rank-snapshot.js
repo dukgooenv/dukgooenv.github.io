fe5e5d3ca9a674e6136cc75f14c71c415d74e7b7
dukgooenv <275051697+dukgooenv@users.noreply.github.com>
2026-04-25 06:07:54 +0000

    Update rank snapshot

--- rank-snapshot.js
+++ rank-snapshot.js
@@ -1,15 +1,15 @@
 window.RANK_SNAPSHOT = {
-  savedAt: "2026-04-24T15:31:17+09:00",
+  savedAt: "2026-04-25T15:07:53+09:00",
   entries: [
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_017_webp", count: 1 },
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_011_webp", count: 1 },
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_010_webp", count: 1 },
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_009_webp", count: 1 },
-    { key: "Professional_2018_NexonKorea_2018_Nexon_AscendantOne_042_webp", count: 1 },
-    { key: "Professional_2018_NexonKorea_2018_Nexon_AscendantOne_023_webp", count: 1 },
-    { key: "Professional_2018_NexonKorea_2018_Nexon_AscendantOne_017_webp", count: 1 },
-    { key: "Professional_2017_XLGAMES_2017_XLGAMES_X3_001_webp", count: 1 },
-    { key: "Personal_20150914_Dungeon_2015_Forsaken_Dungeon_001_webp", count: 1 },
-    { key: "Personal_20111008_PPSh-41_2011_PPSH41_001_webp", count: 1 }
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_017_webp", count: 3 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_010_webp", count: 3 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_009_webp", count: 3 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_002_webp", count: 3 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_001_webp", count: 3 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_011_webp", count: 2 },
+    { key: "Professional_2018_NexonKorea_2018_Nexon_AscendantOne_042_webp", count: 2 },
+    { key: "ReLighting_20240912_BMW_M4GTS_BMW_M4GTS_02_webp", count: 1 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_028_webp", count: 1 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_008_webp", count: 1 }
   ]
 };

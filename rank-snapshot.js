71e93fab55278131e53f1060bd6d7238b0c01a90
dukgooenv <275051697+dukgooenv@users.noreply.github.com>
2026-05-31 04:24:44 +0000

    Update rank snapshot

--- rank-snapshot.js
+++ rank-snapshot.js
@@ -1,15 +1,15 @@
 window.RANK_SNAPSHOT = {
-  savedAt: "2026-05-30T12:59:08+09:00",
+  savedAt: "2026-05-31T13:24:43+09:00",
   entries: [
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_009_webp", count: 5 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_002_webp", count: 5 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_001_webp", count: 5 },
     { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_017_webp", count: 4 },
     { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_010_webp", count: 4 },
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_009_webp", count: 4 },
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_002_webp", count: 4 },
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_001_webp", count: 4 },
     { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_011_webp", count: 3 },
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_036_webp", count: 2 },
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_008_webp", count: 2 },
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_006_webp", count: 2 },
-    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_005_webp", count: 2 }
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_008_webp", count: 3 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_006_webp", count: 3 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_005_webp", count: 3 },
+    { key: "Professional_2021_NCSOFT_2021_NCSOFT_ProjectM_003_webp", count: 3 }
   ]
 };
